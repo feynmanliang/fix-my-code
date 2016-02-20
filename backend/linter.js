5a93f94a63fb8879fa4af7e4da267ccb1723b9e0
Feynman Liang <feynman.liang@gmail.com>
2016-02-20 13:52:21 +0000

    Adds linter

---
+++ backend/linter.js
@@ -0,0 +1,66 @@
+var Promise = require("bluebird");
+var request = Promise.promisifyAll(require("request"));
+
+var Checker = require("jscs");
+var checker = new Checker();
+checker.registerDefaultRules();
+
+// Configure the checker with an options object
+checker.configure({
+    "requireCurlyBraces": [
+        "if",
+        "else",
+        "for"
+    ]
+});
+
+// Use the jQuery preset
+checker.configure({
+    preset: "jquery"
+});
+
+// Use the Google preset, but override or remove some options
+checker.configure({
+    preset: "google",
+    disallowMultipleLineBreaks: null, // or false
+    validateIndentation: "\t"
+});
+
+
+var express = require('express');
+var app = express();
+
+app.get('/lint', function (req, res) {
+  var ghPath = req.query['ghpath'];
+  request.getAsync('https://raw.githubusercontent.com/' + ghPath)
+    .then(function(response) {
+      var code = response.body;
+      var results = checker.checkString(code);
+      var errors = results.getErrorList();
+
+      console.log(response.body)
+
+      var result = {
+        code: code,
+        errors: errors,
+      };
+
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(result));
+
+      //console.log(results.getErrorList())
+
+      //results.getErrorList().forEach(function(error) {
+      //var colorizeOutput = true;
+      //console.log(results.explainError(error, colorizeOutput) + "\n");
+      //});
+    })
+    .catch(function(err) {
+      console.log("Error: " + JSON.stringify(err));
+    });
+});
+
+app.listen(3000, function () {
+  console.log('Linter listening on localhost:3000/lint!');
+});
+
